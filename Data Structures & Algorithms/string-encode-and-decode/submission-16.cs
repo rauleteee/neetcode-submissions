757a98b5486aa1f34ae3bba3ced428787dd11243
Raul <91801890+rauleteee@users.noreply.github.com>
2026-09-03 17:17:15 +0200

    Add: string-encode-and-decode - submission-16

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-16.cs
@@ -0,0 +1,37 @@
+public class Solution {
+
+    public string Encode(IList<string> strs) {
+        // o(n), ["code", "co#de"] ->[Length][Delimiter][String] s = "4#code5#co#de"
+        StringBuilder sb = new StringBuilder();
+        foreach (string s in strs) {
+            sb.Append(s.Length);   // how long this string is
+            sb.Append('#');        // marker separating the length from the content
+            sb.Append(s);          // the actual string
+        }
+        return sb.ToString();
+        
+    }
+
+    public List<string> Decode(string s) {
+        List<string> result = new List<string>();
+        int i = 0;
+        while (i < s.Length) {
+            // Read digits until we hit '#' → that's the length
+            int j = i;
+            while (s[j] != '#') {
+                j++; // number of the digits of the length
+            }
+            // Take the length without
+            int length = int.Parse(s.Substring(i, j - i));
+
+            // The string starts right after '#' and runs 'length' chars
+            string str = s.Substring(j + 1, length);
+            result.Add(str);
+
+            // Jump past this whole chunk to the next one
+            // next chunk will start in length + number of digits of the length(j) + delimiter
+            i = j + 1 + length;
+        }
+        return result;
+    }
+}
